99ba5d094c0d407cf1667582d4989726cf740bcf
unknown <hybiekay2@gmail.com>
2024-07-24 20:56:23 +0100

    first commit2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,14 @@
     <nav class="bg-primary shadow p-4">
         <div class="container mx-auto flex justify-between items-center">
             <a href="#" class="text-white text-2xl font-bold">Euforia</a>
-            <ul class="flex space-x-6">
+            <div class="block lg:hidden">
+                <button id="menu-button" class="text-white focus:outline-none">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                    </svg>
+                </button>
+            </div>
+            <ul id="menu" class="hidden lg:flex space-x-6">
                 <li><a href="#features" class="text-white hover:underline">Features</a></li>
                 <li><a href="#pricing" class="text-white hover:underline">Pricing</a></li>
                 <li><a href="#testimonials" class="text-white hover:underline">Testimonials</a></li>
@@ -28,5 +35,14 @@
             </ul>
         </div>
     </nav>
+
+    <script>
+        const menuButton = document.getElementById('menu-button');
+        const menu = document.getElementById('menu');
+
+        menuButton.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+        });
+    </script>
 </body>
 </html>
